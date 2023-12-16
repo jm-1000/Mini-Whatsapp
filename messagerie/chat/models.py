a01dfc1b86fd89975a47d6659049d121a1373309
jm-1000 <153498495+jm-1000@users.noreply.github.com>
2023-12-16 03:53:10 +0000

    Création de modèles et tests

--- messagerie/chat/models.py
+++ messagerie/chat/models.py
@@ -8,15 +8,35 @@ class Utilisateur(models.Model):
     num_tel = models.CharField(max_length=15, blank=False)
     info = models.TextField(blank=True, max_length=200)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['nom', 'num_tel'], name="%(app_label)s_%(class)s_unique")
+        ]
+
     def __str__(self):
         return self.nom
 
+
 class Message(models.Model):
     texte = models.TextField(blank=False, max_length=2000)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
     heure = models.TimeField(auto_now_add=True)
-    utilisateur = models.ForeignKey('Utilisateur', on_delete=models.CASCADE)
+    util = models.ForeignKey('Utilisateur', null=True, on_delete=models.SET_NULL)
+    chat = models.ForeignKey('Chat', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.texte[:100]
+
+
+class Chat(models.Model):
+    util_1 = models.ForeignKey('Utilisateur', to_field='id', null=True, on_delete=models.DO_NOTHING, related_name="chat_util_1")
+    util_2 = models.ForeignKey('Utilisateur', to_field='id', null=True, on_delete=models.DO_NOTHING, related_name="chat_util_2")
+   
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['util_1', 'util_2'], name="%(app_label)s_%(class)s_unique")
+        ]
+
+    def __str__(self):
+        return f"{self.util_1} - {self.util_2}"
     
--- messagerie/chat/tests.py
+++ messagerie/chat/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .models import *
+import random
 
 # Create your tests here.
+class creer_instances(TestCase):
+    ju=Utilisateur(nom='juari',num_tel='+12345456',info='hi people!')
+    man=Utilisateur(nom='manuel',num_tel='+00999999',info='ola people!')
+    fra=Utilisateur(nom='francis',num_tel='+0544999',info='salut!')
+    ju.save()
+    man.save()
+    fra.save()
+    c1=Chat(util_1=ju,util_2=man)
+    c2=Chat(util_1=fra,util_2=man)
+    c3=Chat(util_1=ju,util_2=fra)
+    c1.save()
+    c2.save()
+    c3.save()
+    # m1=Message(texte='msg 1',util=ju,chat=c1)
+    # m2=Message(texte='msg 2',util=man,chat=c1)
+    # m1.save()
+    # m2.save()
+    getRandom = lambda x: x[random.randint(0,len(x)-1)]
+    for i in range(10):
+        u = getRandom([man,ju,fra])
+        Message(texte=f'{u} : msg {i}',util=u, chat=getRandom([c1,c2,c3])).save()
+    
+    print()
+
+
+
+
+
+    # alias test="rm db.sqlite3;python3 manage.py makemigrations;python3 manage.py migrate >/tmp/.null;echo '';python3 manage.py test"
