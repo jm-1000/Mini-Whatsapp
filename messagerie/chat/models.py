50ce476818aa1d2c129cd8fe9f92fc5834d68c56
jm-1000 <153498495+jm-1000@users.noreply.github.com>
2023-12-16 16:03:14 +0000

    Tests et models v2

--- messagerie/chat/models.py
+++ messagerie/chat/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from datetime import date
-from time import asctime
 
 # Create your models here.
 class Utilisateur(models.Model):
@@ -21,7 +20,7 @@ class Message(models.Model):
     texte = models.TextField(blank=False, max_length=2000)
     date = models.DateField(auto_now_add=True)
     heure = models.TimeField(auto_now_add=True)
-    util = models.ForeignKey('Utilisateur', null=True, on_delete=models.SET_NULL)
+    util = models.ForeignKey('Utilisateur', on_delete=models.CASCADE)
     chat = models.ForeignKey('Chat', on_delete=models.CASCADE)
 
     def __str__(self):
@@ -29,8 +28,8 @@ class Message(models.Model):
 
 
 class Chat(models.Model):
-    util_1 = models.ForeignKey('Utilisateur', to_field='id', null=True, on_delete=models.DO_NOTHING, related_name="chat_util_1")
-    util_2 = models.ForeignKey('Utilisateur', to_field='id', null=True, on_delete=models.DO_NOTHING, related_name="chat_util_2")
+    util_1 = models.ForeignKey('Utilisateur', to_field='id', on_delete=models.CASCADE, related_name="chat_util_1")
+    util_2 = models.ForeignKey('Utilisateur', to_field='id', on_delete=models.CASCADE, related_name="chat_util_2")
    
     class Meta:
         constraints = [
@@ -39,4 +38,7 @@ class Chat(models.Model):
 
     def __str__(self):
         return f"{self.util_1} - {self.util_2}"
+    
+    def get_messages(self):
+        return Message.objects.filter(chat=self)
     
--- messagerie/chat/tests.py
+++ messagerie/chat/tests.py
@@ -3,32 +3,37 @@ from .models import *
 import random
 
 # Create your tests here.
-class creer_instances(TestCase):
+plen = lambda x:print(len(x),'\n')
+class creer_objects(TestCase):
     ju=Utilisateur(nom='juari',num_tel='+12345456',info='hi people!')
     man=Utilisateur(nom='manuel',num_tel='+00999999',info='ola people!')
     fra=Utilisateur(nom='francis',num_tel='+0544999',info='salut!')
+    jm=Utilisateur(nom='jmf',num_tel='+0544999345',info='bonjour!')
     ju.save()
+    jm.save()
     man.save()
     fra.save()
     c1=Chat(util_1=ju,util_2=man)
     c2=Chat(util_1=fra,util_2=man)
     c3=Chat(util_1=ju,util_2=fra)
+    c4=Chat(util_1=man,util_2=jm)
+    c4.save()
     c1.save()
     c2.save()
     c3.save()
-    # m1=Message(texte='msg 1',util=ju,chat=c1)
-    # m2=Message(texte='msg 2',util=man,chat=c1)
-    # m1.save()
-    # m2.save()
     getRandom = lambda x: x[random.randint(0,len(x)-1)]
-    for i in range(10):
-        u = getRandom([man,ju,fra])
-        Message(texte=f'{u} : msg {i}',util=u, chat=getRandom([c1,c2,c3])).save()
-    
-    print()
+    for c in [c1,c1,c1,c2,c2,c3,c3,c4,c4]:
+        u = getRandom([c.util_1,c.util_2])
+        Message(texte=f'{u} : msg {c}',util=u, chat=c).save()
+    # plen(c1.get_messages())
+
+class remover_objects(TestCase):
+    Message.objects.get(id=1).delete()
+    Chat.objects.get(id=2).delete()
+    Utilisateur.objects.get(id=4).delete()
 
 
 
 
 
-    # alias test="rm db.sqlite3;python3 manage.py makemigrations;python3 manage.py migrate >/tmp/.null;echo '';python3 manage.py test"
+    # alias test="rm db.sqlite3 2>/dev/null;python3 manage.py makemigrations;python3 manage.py migrate >/tmp/.null;echo '';python3 manage.py test"
